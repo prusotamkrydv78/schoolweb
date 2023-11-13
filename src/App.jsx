252f28db3a16a54497a0b4a9b0570b8de1cdfbdf
prusotamkrydv78 <prusotamkumaryadav78@gmail.com>
2023-11-13 09:44:07 +0545

    navbar setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from './Components/NavBar/NavBar'
+const App = () => {
+  return (
+    <>
+      <NavBar/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+       <div className="navbar">
+        <ul>
+            
+        </ul>
+       </div>
+    </div>
+  )
+}
+
+export default NavBar
